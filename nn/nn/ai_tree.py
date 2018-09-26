a1ce3994d0d899eae9c0ac0051f6170ee2f4bd42
Andrzej Dabski <jedrus1133@gmail.com>
2018-09-26 11:15:49 +0200

    templates

--- nn/nn/ai_tree.py
+++ nn/nn/ai_tree.py
@@ -1,32 +1,6 @@
-#import random
-#import numpy as np
-
-def gen_tab():
-    tab = []
-    max_size = 1000
-    for _ in range(100000):
-        for line in np.random.randint(0, max_size, (10,6)):
-            tab.append(line)
-    return tab
-
-def validate(t):
-    tab = []
-    for ax, ay, bux, buy, bdx, bdy in t:
-        tab.append(ax >= bux\
-            and ax <= bdx\
-            and ay >= buy\
-            and ay <= bdy )
-    return tab
-
-#X = gen_tab()
-#Y = validate(X)
-
 from sklearn import tree
-#clf = tree.DecisionTreeClassifier()
-#clf = clf.fit(X, Y)
-
 from sklearn.externals import joblib
-#joblib.dump(clf, 'new_ai.pkl')
+
 clf = joblib.load('new_ai.pkl')
 
 def predict(x=[0],y=[0], bx1=[0], by1=[0], bx2=[0], by2=[0]):
--- nn/nn/urls.py
+++ nn/nn/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.get_page),
+    path('', TemplateView.as_view(template_name="client.html")),
     path('req', views.ai_response),
     path('admin/', admin.site.urls),
 ]
