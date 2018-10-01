129fb5abf23c4be9979c617056595773177d055f
Andrzej Dabski <jedrus1133@gmail.com>
2018-10-01 11:55:25 +0200

    frontend

--- nn/nn/ai_tree.py
+++ nn/nn/ai_tree.py
@@ -9,7 +9,7 @@ def predict(x=[0],y=[0], bx1=[0], by1=[0], bx2=[0], by2=[0]):
     y = int(y[0])
 
     for i in range(len(bx1)):
-        tab = []
+        tab = [x,y]
         tab.append(int(bx1[i]))
         tab.append(int(by1[i]))
         tab.append(int(bx2[i]))
--- nn/nn/views.py
+++ nn/nn/views.py
@@ -5,4 +5,7 @@ from nn import ai_tree
 
 def ai_response(request):
     odp = ai_tree.predict(**request.GET)
-    return HttpResponse(odp)
+    s = str(odp[0])
+    for x in odp[1:]:
+    	s += f",{x}"
+    return HttpResponse(s)
